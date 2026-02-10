1b902784fc82cc15d1b2a3f41e56aa2e1355cd18
JS-practice-questions <gshubh558@gmail.com>
2026-02-10 21:19:12 +0530

    add new questions2

--- questions2.js
+++ questions2.js
@@ -7,44 +7,214 @@
 
 
 // Print first element of array
-let arr=[1,3,4,2];
-console.log(arr[0]);
+// let arr=[1,3,4,2];
+// console.log(arr[0]);
 
 
 // Print last element of array
-
+// let arr=[1,2,3,4];
+// console.log(arr[3])
 
 
 // Update second element of array
-
+// let arr=[1,2,3,4];
+// arr[1]=5;
+// console.log(arr)
 
 
 // Count total elements in array
-
+// let arr=[1,2,3,4];
+// let count=0;
+// for(let i=0; i<arr.length; i++){
+//     count++;
+// }
+// console.log(count)
 
 
 // Print all elements using loop
-
+// let arr=[12,13,14,15];
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
 
 
 // Find average of array elements
-
+// let arr=[1,2,3,4];
+// let sum=0;
+// let count=0;
+// for(let i=0; i<arr.length; i++){
+//     sum+=arr[i];
+//     count++
+// }
+// console.log(sum/count)
 
 
 // Count even numbers in array
-
+// let arr=[1,2,3,4,5,6];
+// let count=0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]%2===0){
+//         count++
+//     }
+// }
+// console.log(count)
 
 
 // Count odd numbers in array
+// let arr=[1,2,3,4,5,6];
+// let count=0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]%2!==0){
+//         count++
+//     }
+// }
+// console.log(count)
 
 
 
-// Check if array contains a value
-
+// Check if array contains a value                    /////////////
+// let arr=[1,2,3,4];
+// let value=5;
+// let found=false;
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i]===value){
+//         found=true;
+//         break;
+//     }
+// }
+// console.log(found); 
 
 
 // Copy one array into another
-
+// let arr1=[1,2,3,4];
+// let arr2=[5,6,7,8];
+// for(let i=0; i<arr1.length; i++){
+//     arr2[i]=arr1[i];
+// }
+// console.log(arr2)
 
 
 // Reverse array using loop
+// let arr=[2,3,4,5];
+// let reverse=[]
+// for(let i=arr.length-1; i>=0; i--){
+//     reverse.push(arr[i]);
+// }
+// console.log(reverse);
+
+
+// Create an array with 5 numbers
+// let arr=[1,2,3,4,5];
+// console.log(arr)
+
+
+// Create an array with mixed data types
+// let arr=[1,2,"shubh"];
+// console.log(arr)
+
+
+// Print the third element of an array
+// let arr=[1,2,3,4];
+// console.log(arr[2])
+
+
+// Print the second last element of an array
+// let arr=[1,2,3,4,5];
+// console.log(arr[arr.length-2])
+
+
+// Change the first element of an array
+// let arr=[1,2,3,4,5];
+// arr.splice(0,1,5)
+// console.log(arr)
+
+
+// Change the last element of an array
+// let arr=[1,2,3,4,5];
+// arr.splice(-1,1,1);
+// console.log(arr)
+
+
+// Find index of a given element manually (using loop)  /////////////////
+// let arr=[11,12,13,14];
+// let num=13;
+// let index=-1
+// for(i=0; i<arr.length; i++){
+//     if(arr[i]===num){
+//         index=i
+//         break;
+//     }
+// }
+// console.log(index)
+
+
+// Count how many elements are greater than 10
+// let arr=[1,2,22,6,32,23];
+// let count=0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]>10){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+// Count how many elements are less than 0
+// let arr=[1,-2,-22,-6,-32,23];
+// let count=0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]<0){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+// Print only positive numbers from array
+// let arr=[1,-2,22,-6,-32,23];
+// let positive=[]
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]>0){
+//         positive.push(arr[i]);
+//     }
+// }
+// console.log(positive)
+
+
+// Print only negative numbers from array
+
+
+
+// Find sum of elements at even indexes
+
+
+
+// Find sum of elements at odd indexes
+
+
+
+// Print array elements in reverse order
+
+
+
+// Check if array is empty
+
+
+
+// Compare two arrays element by element
+
+
+
+// Find difference between array length and given number
+
+
+
+// Print duplicate elements in array
+
+
+
+// Remove an element at a specific index (using loop)
+
+
+
+// Insert an element at a specific index
