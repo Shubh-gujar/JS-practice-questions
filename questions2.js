10eea40dc2cf1653434e534b6d419a641c93b975
JS-practice-questions <gshubh558@gmail.com>
2026-02-10 15:48:25 +0530

    new file

---
+++ questions2.js
@@ -0,0 +1,50 @@
+// Create an empty array
+// let arr=[];
+
+
+// Create array of strings
+// let arr=["shubh","juned"];
+
+
+// Print first element of array
+let arr=[1,3,4,2];
+console.log(arr[0]);
+
+
+// Print last element of array
+
+
+
+// Update second element of array
+
+
+
+// Count total elements in array
+
+
+
+// Print all elements using loop
+
+
+
+// Find average of array elements
+
+
+
+// Count even numbers in array
+
+
+
+// Count odd numbers in array
+
+
+
+// Check if array contains a value
+
+
+
+// Copy one array into another
+
+
+
+// Reverse array using loop
